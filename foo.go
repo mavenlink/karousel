73d555480e6b40cf19d180ce5cd012d2c0d58eac
Armando Cerna <armando@mavenlink.com>
2016-08-02 11:27:30 -0700

    Connecting to kube, need to figure out self signed certs

--- foo.go
+++ foo.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+  "fmt"
   client "k8s.io/kubernetes/pkg/client/unversioned"
+  restclient "k8s.io/kubernetes/pkg/client/restclient"
   "k8s.io/kubernetes/pkg/api"
 )
 
 func main() {
-  config := &client.Config{
-    Host:     "http://localhost:8080",
-    Username: "test",
-    Password: "password",
+  config := &restclient.Config{
+    Host:     "https://192.168.99.112:8443",
   }
-  client, err := client.New(config)
+  kubeclient, err := client.New(config)
   if err != nil {
-    // handle error
+    fmt.Printf("%s\n",err)
   }
-  pods, err := client.Pods(api.NamespaceDefault).List(api.ListOptions{})
+  podlist, err := kubeclient.Pods(api.NamespaceDefault).List(api.ListOptions{})
   if err != nil {
-    // handle error
+    fmt.Printf("%s\n",err)
   }
+  fmt.Printf("%s\n",podlist.Items)
+  fmt.Printf("%d\n",len(podlist.Items))
 }
