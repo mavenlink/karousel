3a9bf430da2bd47f2a61ec90daa703207df75d47
Armando Cerna <armandocerna@gmail.com>
2016-08-02 23:08:42 -0700

    Better error handling, fix formatting to go spec

--- foo.go
+++ foo.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-  "fmt"
-  client "k8s.io/kubernetes/pkg/client/unversioned"
-  restclient "k8s.io/kubernetes/pkg/client/restclient"
-  "k8s.io/kubernetes/pkg/api"
+	"fmt"
+	"k8s.io/kubernetes/pkg/api"
+	restclient "k8s.io/kubernetes/pkg/client/restclient"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
+	"log"
 )
 
 func main() {
-  config := &restclient.Config{
-    Host:     "http://192.168.99.112:8081",
-    Username: "admin",
-    Password: "merge4justice",
-    Insecure:  true,
-  }
-  kubeclient, err := client.New(config)
-  if err != nil {
-    fmt.Printf("%s\n",err)
-  }
-  podlist, err := kubeclient.Pods(api.NamespaceDefault).List(api.ListOptions{})
-  if err != nil {
-    fmt.Printf("%s\n",err)
-  }
-  fmt.Printf("Pods: %s\n",podlist.Items)
-  fmt.Printf("Connection: %s\n",kubeclient)
-  fmt.Printf("%d\n",len(podlist.Items))
+	config := &restclient.Config{
+		Host:     "http://192.168.99.100:8081",
+		Username: "admin",
+		Password: "merge4justice",
+		Insecure: true,
+	}
+	kubeclient, err := client.New(config)
+	if err != nil {
+		log.Fatalln("Can't connect to Kubernetes API:", err)
+	}
+	podlist, err := kubeclient.Pods(api.NamespaceDefault).List(api.ListOptions{})
+	if err != nil {
+		log.Fatalln("Can't get pods:", err)
+	}
+	fmt.Printf("Pods: %s\n", podlist.Items)
+	// fmt.Printf("Connection: %s\n", kubeclient)
+	// fmt.Printf("%d\n", len(podlist.Items))
 }
