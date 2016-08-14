2a4fd5157d5022403385cea4f2c76df467fe93ad
Armando Cerna <armandocerna@gmail.com>
2016-08-13 20:24:07 -0700

    Working pod delete reading from TTL

--- main.go
+++ main.go
@@ -1,84 +1,65 @@
 package main
 
 import (
-	// "encoding/json"
+	"flag"
 	"fmt"
-	// "reflect"
-
-	flag "github.com/spf13/pflag"
-	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/client/restclient"
-	client "k8s.io/kubernetes/pkg/client/unversioned"
-	kubectl_util "k8s.io/kubernetes/pkg/kubectl/cmd/util"
-	"strconv"
-	// "log"
-	// "net/http"
+	"log"
 	"os"
+	"strconv"
 	"time"
+
+	// "github.com/golang/glog"
+	"github.com/spf13/pflag"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/client/unversioned"
+	kubectl_util "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 )
 
 var (
-	flags     = flag.NewFlagSet("", flag.ContinueOnError)
-	inCluster = flags.Bool("use-kubernetes-cluster-service", false, `If true, use the built in kubernetes
-        cluster for creating the client`)
-	host = flags.String("host", "",
-		`K8s host`)
-	insecure = flags.Bool("insecure", false,
-		`Enforce CA check for cert`)
+	flags = pflag.NewFlagSet("", pflag.ExitOnError)
 )
 
-type itemdata [][]string
-
-func getPodsToKill() {
+func main() {
+	flags.AddGoFlagSet(flag.CommandLine)
+	flags.Parse(os.Args)
 	clientConfig := kubectl_util.DefaultClientConfig(flags)
 
-	// flags.Parse(os.Args)
-
-	config := &restclient.Config{
-		Host:     *host,
-		Insecure: *insecure,
+	config, err := clientConfig.ClientConfig()
+	if err != nil {
+		log.Fatalf("error connecting to the client: %v", err)
 	}
 
-	var err error
+	kubeClient, err := unversioned.New(config)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
 
-	config, err = clientConfig.ClientConfig()
-	check(err)
-	kubeClient, err := client.New(config)
-	check(err)
 	podlist, err := kubeClient.Pods(api.NamespaceDefault).List(api.ListOptions{})
-	check(err)
+	if err != nil {
+		log.Fatalf("failed list pods: %v", err)
+	}
 
 	for _, pod := range podlist.Items {
 		currentTime := time.Now()
 		startTime := pod.Status.StartTime
 		ttl, err := strconv.ParseFloat(pod.Labels["ttl"], 64)
-		check(err)
+		if err != nil {
+			log.Printf("failed parse label ttl: %v", err)
+			continue
+		}
 
 		podAge := currentTime.Sub(startTime.Time)
 		podAgeHours := podAge.Hours()
-		if podAgeHours > ttl {
-			fmt.Println("Attempting to kill pod", pod.Name, "it is older then", ttl, "hours")
-			killPods(pod.Name)
-		} else {
-			fmt.Println("Pod", pod.Name, "is younger then", ttl, "hours")
+		if podAgeHours <= ttl {
+			log.Println("Pod", pod.Name, "is younger then", ttl, "hours")
+			continue
+		}
+
+		fmt.Println("Attempting to kill pod", pod.Name, "it is older then", ttl, "hours")
+		err = kubeClient.Pods(pod.Namespace).Delete(pod.Name, &api.DeleteOptions{})
+		if err != nil {
+			log.Printf("Pod %v was deleted\n", pod)
 		}
 	}
-}
-
-func killPods(pod string) {
-	fmt.Println("Killed", pod, "!")
-}
-
-func check(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-
-func main() {
-	// http.HandleFunc("/", getPods) // set route to get pods
-	// err := http.ListenAndServe(":9090", nil)
-	// check(err)
-	getPodsToKill()
 }
